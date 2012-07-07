26b6330bf6d7d53c0884f70510aae6fdccaa2e30
Я Чайка <mellanie335@gmail.com>
2012-07-07 14:43:15 +0900

    Add Empty Class CFStreamConnection
    Signed-off-by: Я Чайка <mellanie335@gmail.com>

---
+++ CFStreamConnection.m
@@ -0,0 +1,13 @@
+//
+//  CFStreamConnection.m
+//  
+//
+//  Created by Чайка on 7/7/12.
+//  Copyright (c) 2012 iom. All rights reserved.
+//
+
+#import "CFStreamConnection.h"
+
+@implementation CFStreamConnection
+
+@end
