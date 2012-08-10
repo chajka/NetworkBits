2b4d644b80282eb434463d78acc4b3536598f6f6
Я Чайка <mellanie335@gmail.com>
2012-08-10 12:09:13 +0900

    Add empty HTTPConnection’s delegate class HTTPCookieConnection.

---
+++ HTTPCookieConnection.h
@@ -0,0 +1,13 @@
+//
+//  HTTPCookieConnection.h
+//  Charleston
+//
+//  Created by Чайка on 8/10/12.
+//  Copyright (c) 2012 iom. All rights reserved.
+//
+
+#import "HTTPConnection.h"
+
+@interface HTTPCookieConnection : HTTPConnection
+
+@end
---
+++ HTTPCookieConnection.m
@@ -0,0 +1,13 @@
+//
+//  HTTPCookieConnection.m
+//  Charleston
+//
+//  Created by Чайка on 8/10/12.
+//  Copyright (c) 2012 iom. All rights reserved.
+//
+
+#import "HTTPCookieConnection.h"
+
+@implementation HTTPCookieConnection
+
+@end
