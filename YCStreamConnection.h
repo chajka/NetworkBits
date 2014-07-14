27f45cb57bf232f68171f364bb8290d36bdac59c
Я Чайка <mellanie335@gmail.com>
2014-07-14 20:37:06 +0900

    Add empty class YCStreamConnectionGCD into repo.

---
+++ YCStreamConnection.h
@@ -0,0 +1,13 @@
+//
+//  YCStreamConnection.h
+//  NicoLiveAlert
+//
+//  Created by Чайка on 7/14/14.
+//  Copyright (c) 2014 Instrumentality of mankind. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface YCStreamConnection : NSObject
+
+@end
---
+++ YCStreamConnection.m
@@ -0,0 +1,13 @@
+//
+//  YCStreamConnection.m
+//  NicoLiveAlert
+//
+//  Created by Чайка on 7/14/14.
+//  Copyright (c) 2014 Instrumentality of mankind. All rights reserved.
+//
+
+#import "YCStreamConnection.h"
+
+@implementation YCStreamConnection
+
+@end
